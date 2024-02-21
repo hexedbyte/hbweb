975059dc20855154a96ca1f15920df985632379a
hexedbyte <hexedbyte@gmail.com>
2024-02-21 10:47:58 +0100

    build

--- src/theme.tsx
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from '@mui/joy/styles';
-import '@fontsource/inter';
-
-export default extendTheme({
-    fontFamily: {
-        display: "'Inter', var(--joy-fontFamily-fallback)",
-        body: "'Inter', var(--joy-fontFamily-fallback)",
-    },
-});
